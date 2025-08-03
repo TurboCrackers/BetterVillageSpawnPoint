b26735260bfecd1db9f97fd3bc019a3254aaa06f
TurboCrackers <LauraEA1997@gmail.com>
2025-08-03 13:34:25 -0700

    Ported to 1.21.6

--- forge/src/main/java/com/turbocrackers/bettervillagespawnpoint/BetterVillageSpawnPointForge.java
+++ forge/src/main/java/com/turbocrackers/bettervillagespawnpoint/BetterVillageSpawnPointForge.java
@@ -7,7 +7,7 @@ import net.minecraft.server.level.ServerLevel;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.event.server.ServerStartedEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.eventbus.api.listener.SubscribeEvent;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 
