420c1087044701f2c1749dc31a0e8ff2ff41b2af
TurboCrackers <LauraEA1997@gmail.com>
2025-08-10 01:48:49 -0700

    Update VillageLocator.java

--- common/src/main/java/com/turbocrackers/bettervillagespawnpoint/util/VillageLocator.java
+++ common/src/main/java/com/turbocrackers/bettervillagespawnpoint/util/VillageLocator.java
@@ -16,6 +16,7 @@ import net.minecraft.world.level.ChunkPos;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.StructureManager;
+import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.BushBlock;
