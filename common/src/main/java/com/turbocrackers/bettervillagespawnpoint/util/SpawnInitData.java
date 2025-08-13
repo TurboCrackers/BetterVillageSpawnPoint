363f11156b9feaeedaf08074534e585d35dd7d72
TurboCrackers <LauraEA1997@gmail.com>
2025-08-12 23:15:51 -0700

    Optimization/stability pass

--- common/src/main/java/com/turbocrackers/bettervillagespawnpoint/util/SpawnInitData.java
+++ common/src/main/java/com/turbocrackers/bettervillagespawnpoint/util/SpawnInitData.java
@@ -1,7 +1,8 @@
 package com.turbocrackers.bettervillagespawnpoint.util;
 
+import com.turbocrackers.bettervillagespawnpoint.Constants;
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.ListTag;
 import net.minecraft.nbt.StringTag;
@@ -9,11 +10,9 @@ import net.minecraft.nbt.Tag;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.saveddata.SavedData;
-import org.jetbrains.annotations.NotNull;
-
 import java.util.ArrayList;
-import java.util.Optional;
 
 public class SpawnInitData extends SavedData
 {
@@ -36,6 +35,7 @@ public class SpawnInitData extends SavedData
         UNKNOWN_LOADING_ERROR
     }
 
+    public boolean m_Initialized = false;
     public VillageSpawnPointState m_State = VillageSpawnPointState.NOT_STARTED;
     public BlockPos m_VillageSpawnPos = BlockPos.ZERO;
     public Boolean m_SentFailureMessage = false;
@@ -43,19 +43,15 @@ public class SpawnInitData extends SavedData
     public ArrayList<Block> m_BlockWhitelist = new ArrayList<Block>();
     public BlockPos m_VillagePos = BlockPos.ZERO;
     public String m_VillageID = "";
+    public static String SAVE_DATA_ID = "bettervillagespawnpoint_spawn_data";
 
-    public SpawnInitData()
-    {
-        // Default constructor
-    }
-
-    public static SpawnInitData load(CompoundTag tag)
-    {
+    public static SpawnInitData load(CompoundTag tag) {
+        Constants.LOG.info("[Better Village Spawn Point] Spawn data loaded.");
         SpawnInitData data = new SpawnInitData();
-        data.m_State = VillageSpawnPointState.valueOf(String.valueOf(tag.getString("VillageSpawnPointState")));
+        data.m_State = VillageSpawnPointState.valueOf(tag.getString("VillageSpawnPointState"));
         data.m_VillageSpawnPos = new BlockPos(tag.getInt("VillageSpawnPosX"), tag.getInt("VillageSpawnPosY"), tag.getInt("VillageSpawnPosZ"));
         data.m_VillagePos = new BlockPos(tag.getInt("VillagePosX"), tag.getInt("VillagePosY"), tag.getInt("VillagePosZ"));
-        data.m_VillageID = String.valueOf(tag.getString("VillageID"));
+        data.m_VillageID = tag.getString("VillageID");
         data.m_SentFailureMessage = tag.getBoolean("VillageSpawnFailureMessageSent");
         data.m_FailureReason = VillageSpawnPointFailureReason.valueOf(tag.getString("VillageSpawnFailureReason"));
 
@@ -66,18 +62,18 @@ public class SpawnInitData extends SavedData
             for (int i = 0; i < blockListTag.size(); i++) {
                 String blockIdString = blockListTag.getString(i);
                 ResourceLocation blockId = ResourceLocation.tryParse(blockIdString);
-                if( blockId == null )
-                    continue;
+                Block block = BuiltInRegistries.BLOCK.get(blockId);
 
-                Block block = Registry.BLOCK.get(blockId);
-                data.m_BlockWhitelist.add(block);
+                if (block != Blocks.AIR) { // Ensure it's valid
+                    data.m_BlockWhitelist.add(block);
+                }
             }
         }
         return data;
     }
 
     @Override
-    public @NotNull CompoundTag save(CompoundTag tag)
+    public CompoundTag save(CompoundTag tag)
     {
         tag.putString("VillageSpawnPointState", m_State.name());
         tag.putInt( "VillageSpawnPosX", m_VillageSpawnPos.getX() );
@@ -92,15 +88,29 @@ public class SpawnInitData extends SavedData
         ListTag listTag = new ListTag();
         for (Block block : m_BlockWhitelist)
         {
-            ResourceLocation blockId = Registry.BLOCK.getKey(block);
-            listTag.add(StringTag.valueOf(blockId.toString()));
+            ResourceLocation blockId = BuiltInRegistries.BLOCK.getKey(block);
+            if( blockId != null )
+            {
+                listTag.add(StringTag.valueOf(blockId.toString()));
+            }
         }
         tag.put("BlockWhitelist", listTag);
         return tag;
     }
 
+    public static void save(ServerLevel level)
+    {
+        level.getServer().executeBlocking(() ->
+        {
+            SpawnInitData data = get(level);
+            data.setDirty();
+
+            level.getDataStorage().save();
+        });
+    }
+
     public static SpawnInitData get(ServerLevel level)
     {
-        return level.getDataStorage().computeIfAbsent(SpawnInitData::load, SpawnInitData::new, "bettervillagespawnpoint_spawn_data");
+        return level.getDataStorage().computeIfAbsent(SpawnInitData::load, SpawnInitData::new, SAVE_DATA_ID);
     }
 }
